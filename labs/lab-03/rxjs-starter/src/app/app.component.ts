8535ba0de901badb3a42c4b4e8555e8cdecdfb8b
Gerhard <gerhard.jungwirth3@gmail.com>
2023-09-12 13:36:50 +0200

    lab 03

--- labs/lab-03/rxjs-starter/src/app/app.component.ts
+++ labs/lab-03/rxjs-starter/src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { MatSidenavContainer, MatSidenavContent, MatSidenavModule } from '@angular/material/sidenav';
+import { SidenavService } from './shared/sidenav/sidenav.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [NavbarComponent, SidebarComponent, RouterOutlet]
+    imports: [NavbarComponent, SidebarComponent, RouterOutlet, MatSidenavModule, CommonModule]
 })
 export class AppComponent {
   title = 'Food App';
+  ms = inject(SidenavService);
+
+  getWorbenchStyle() {
+    let result = {};
+    this.ms.sideNavVisible.subscribe((visible) => {
+      result = visible
+        ? {
+          'padding-left': '10px',
+        }
+        : {};
+    });
+    return result;
+  }
 }
--- labs/lab-03/rxjs-starter/src/app/shared/navbar/navbar.component.ts
+++ labs/lab-03/rxjs-starter/src/app/shared/navbar/navbar.component.ts
@@ -1,17 +1,20 @@
-import { AsyncPipe, NgFor } from '@angular/common';
+import { AsyncPipe, CommonModule, NgFor } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 import { NavbarService } from './navbar.service';
+import {MatIconModule} from '@angular/material/icon';
+import { SidenavService } from '../sidenav/sidenav.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   standalone: true,
-  imports: [MatToolbarModule, RouterLink, NgFor, AsyncPipe],
+  imports: [MatToolbarModule, RouterLink, NgFor, AsyncPipe, MatIconModule, CommonModule],
 })
 export class NavbarComponent {
   ns = inject(NavbarService);
   items = this.ns.getTopItems();
+  ms = inject(SidenavService);
 }
