4c433e1cb084a8c23a877ec73d229dde478d01a8
Gerhard <gerhard.jungwirth3@gmail.com>
2023-09-13 14:27:08 +0200

    lab8lib setup project

---
+++ labs/lab-08/libs/ux-lib/projects/ux-controls/src/lib/ux-controls.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { UxControlsComponent } from './ux-controls.component';
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+  ],
+  exports: [
+  ]
+})
+export class UxControlsModule { }
